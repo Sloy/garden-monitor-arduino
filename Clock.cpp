3e640ce8e97fcc1151bc079b74900419c6c46a5b
Rafa Vázquez <rafa.vazsan@gmail.com>
2022-02-02 19:34:52 +0100

    Add logs to Clock

--- Clock.cpp
+++ Clock.cpp
@@ -1,22 +1,27 @@
 #include "Clock.h"
+
 #include <NTPClient.h>
 #include <WiFiNINA.h>
+
+#include "config.h"
 
 WiFiUDP ntpUDP;
 NTPClient ntpClient(ntpUDP);
 
-
-void Clock::begin(){
+void Clock::begin() {
     ntpClient.begin();
 }
 
-unsigned long Clock::now(){
- // Update time via NTP if required
-  while (!ntpClient.update()) {
-    yield();
-    ntpClient.forceUpdate();
-  }
-  // Get current timestamp
-  unsigned long ts = ntpClient.getEpochTime();
-  return ts;
+unsigned long Clock::now() {
+    // Update time via NTP if required
+    bool result = ntpClient.update();
+    LOGLN(result);
+    while (!ntpClient.update()) {
+        yield();
+        LOGLN("(Clock) Force update");
+        ntpClient.forceUpdate();
+    }
+    // Get current timestamp
+    unsigned long ts = ntpClient.getEpochTime();
+    return ts;
 }
